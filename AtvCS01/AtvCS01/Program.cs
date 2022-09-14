577831b239a6ba0f346b92f2c027201fae426cfd
osrpato <eugeniovitorsilva@gmail.com>
2022-09-14 17:29:21 -0300

    first commit

---
+++ AtvCS01/AtvCS01/Program.cs
@@ -0,0 +1,17 @@
+﻿class program
+{
+    static void Main()
+    {
+        string[] names = { "Marlos", "Thiago", "Mell" };
+
+        if (names[0] == "Marlos")
+        {
+            Console.WriteLine("Igual");
+        }
+        foreach (string name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+    }
+}
---
+++ AtvCSharp03/AtvCSharp03/Program.cs
@@ -0,0 +1,34 @@
+﻿int num1 = 0; int num2 = 0;
+
+Console.WriteLine(" Calculadora em --> C#\r");
+
+Console.WriteLine("digite um número: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("digite outro número: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("escolha uma opção da lista a seguir:");
+Console.WriteLine("\ta - Adição");
+Console.WriteLine("\ts - Subtração");
+Console.WriteLine("\tm - Multiplicação");
+Console.WriteLine("\td - Divisão");
+Console.Write("escolha um ");
+
+switch (Console.ReadLine())
+{
+    case "a":
+        Console.WriteLine($"seu resultado: {num1} + {num2} = " + (num1 + num2));
+        break;
+    case "s":
+        Console.WriteLine($"seu resultado: {num1} - {num2} = " + (num1 - num2));
+        break;
+    case "m":
+        Console.WriteLine($"seu resultado: {num1} * {num2} = " + (num1 * num2));
+        break;
+    case "d":
+        Console.WriteLine($"seu resultado: {num1} / {num2} = " + (num1 / num2));
+        break;
+}
+Console.Write("pressione qualquer tecla para fechar o terminal...");
+Console.ReadKey();
